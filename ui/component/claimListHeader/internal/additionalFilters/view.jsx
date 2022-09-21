ee6921ca6a55e57311509d1314688200e3207b61
infinite-persistence <inf.persistence@gmail.com>
2022-09-21 13:58:56 +0800

    ClaimListHeader: style checkbox to be more integrated
    Also moved the items into a new "Additional Filters" component, as there will be a few more "Hide xxx" items coming up with memberships.

---
+++ ui/component/claimListHeader/internal/additionalFilters/view.jsx
@@ -0,0 +1,35 @@
+// @flow
+import React from 'react';
+
+import './style.scss';
+import { FormField } from 'component/common/form';
+import * as CS from 'constants/claim_search';
+
+type Props = {
+  filterCtx: any,
+  contentType: string,
+};
+
+function AdditionalFilters(props: Props) {
+  const { filterCtx, contentType } = props;
+
+  return (
+    <div className="additional-filters">
+      <fieldset>
+        <label>{__('Additional Filters')}</label>
+        {filterCtx?.repost && (
+          <FormField
+            label={__('Hide reposts')}
+            name="hide_reposts"
+            type="checkbox"
+            checked={filterCtx.repost.hideReposts}
+            disabled={contentType === CS.CLAIM_REPOST}
+            onChange={() => filterCtx.repost.setHideReposts((prev) => !prev)}
+          />
+        )}
+      </fieldset>
+    </div>
+  );
+}
+
+export default AdditionalFilters;
